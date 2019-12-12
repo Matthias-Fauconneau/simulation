cd03f265057ab88248bf234ae138233f9f341bb6
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2019-12-12 02:06:52 +0100

    Initial commit

---
+++ build.rs
@@ -0,0 +1,7 @@
+//extern crate bindgen;
+fn main() {
+    println!("cargo:rustc-link-lib=umfpack");
+    println!("cargo:rerun-if-changed=wrapper.h");
+    bindgen::Builder::default().header("wrapper.h").parse_callbacks(Box::new(bindgen::CargoCallbacks)).generate().unwrap()
+        .write_to_file(std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("bindings.rs")).unwrap();
+}
