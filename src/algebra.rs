a042ebb8e9431bd76c554497fcb4b7051f31d98a
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2020-01-27 14:05:53 +0100

    Workaround rustc incomplete const generics support

--- src/algebra.rs
+++ src/algebra.rs
@@ -1,45 +1,59 @@
-use std::ops::Mul; use framework::core::Zero;
-
 pub type Len = usize;
-#[derive(Clone)] pub struct Array<T=f32, const N:Len>([T; N]);
+/*#[derive(Clone)] pub struct Array<T=f32, const N:Len>([T; N]);
 impl<T,const N:Len> std::ops::Deref for Array<T,N> { type Target = [T; N]; fn deref(&self) -> &Self::Target { &self.0 } }
 impl<T,const N:Len> std::ops::DerefMut for Array<T,N> { fn deref_mut(&mut self) -> &mut Self::Target { &mut self.0 } }
-
 pub type Idx = usize;
-impl<T:Copy,const N:Len> FnOnce<(Idx,)> for Array<T,N> { type Output=T;  extern "rust-call" fn call_once(self, args: (Idx,)) -> Self::Output { self.call(args) } }
-impl<T:Copy,const N:Len> FnMut<(Idx,)> for Array<T,N> { extern "rust-call" fn call_mut(&mut self, args: (Idx,)) -> Self::Output { self.call(args) } }
-impl<T:Copy,const N:Len> Fn<(Idx,)> for Array<T,N> { extern "rust-call" fn call(&self, args: (Idx,)) -> Self::Output { self[args.0] } }
-
 pub fn collect<T, F:Fn(Idx)->T, const N:Len>(f : F) -> Array<T, N> { Array(framework::core::array::collect(f)) }
-impl<T:Zero, const N : Idx> Zero for Array<T,N> { fn zero() -> Self { collect(|_|Zero::zero()) } }
 
 impl<'a,T:std::ops::AddAssign<&'a T>,const N:Len> std::ops::AddAssign<&'a Array<T,N>> for Array<T,N> {
     fn add_assign(&mut self, other: &'a Self) { for (a, b) in self.iter_mut().zip(other.iter()) { *a += b; } }
 }
 
+impl<T:Copy,const N:Len> FnOnce<(Idx,)> for Array<T,N> { type Output=T;  extern "rust-call" fn call_once(self, args: (Idx,)) -> Self::Output { self.call(args) } }
+impl<T:Copy,const N:Len> FnMut<(Idx,)> for Array<T,N> { extern "rust-call" fn call_mut(&mut self, args: (Idx,)) -> Self::Output { self.call(args) } }
+impl<T:Copy,const N:Len> Fn<(Idx,)> for Array<T,N> { extern "rust-call" fn call(&self, args: (Idx,)) -> Self::Output { self[args.0] } }
+
+use framework::core::Zero;
+impl<T:Zero, const N : Idx> Zero for Array<T,N> { fn zero() -> Self { collect(|_|Zero::zero()) } }*/
+
+#[derive(Clone)] pub struct Array<T=f32>(Vec<T>);
+impl<T> std::ops::Deref for Array<T> { type Target = Vec<T>; fn deref(&self) -> &Self::Target { &self.0 } }
+impl<T> std::ops::DerefMut for Array<T> { fn deref_mut(&mut self) -> &mut Self::Target { &mut self.0 } }
+pub type Idx = usize;
+pub fn collect<T, F:Fn(Idx)->T, const N:Len>(f : F) -> Array<T> { Array((0..N).map(f).collect()) }
+use framework::core::Zero;
+pub fn zero<T:Zero, const N:Len>() -> Array<T> { collect::<_,_,N>(|_|Zero::zero()) }
+impl<T:Copy> FnOnce<(Idx,)> for Array<T> { type Output=T;  extern "rust-call" fn call_once(self, args: (Idx,)) -> Self::Output { self.call(args) } }
+impl<T:Copy> FnMut<(Idx,)> for Array<T> { extern "rust-call" fn call_mut(&mut self, args: (Idx,)) -> Self::Output { self.call(args) } }
+impl<T:Copy> Fn<(Idx,)> for Array<T> { extern "rust-call" fn call(&self, args: (Idx,)) -> Self::Output { self[args.0] } }
+
+use std::ops::Mul;
+
 /// Vector
-pub trait Vector<T=f32,const N:Len> = Fn(Idx)->T;
-pub type BoxVector<'a,T,const N:Len> = crate::compose::BoxFn<'a,(Idx,),T>;
+//pub trait Vector<T=f32,const N:Len> = Fn(Idx)->T;
+pub trait Vector<T=f32> = Fn(Idx)->T;
+//pub type BoxVector<'a,T,const N:Len> = crate::compose::BoxFn<'a,(Idx,),T>;
+pub type BoxVector<'a,T> = crate::compose::BoxFn<'a,(Idx,),T>;
 // impl scalar · vector for Array
 //pub fn mul<'a, T:Copy, const N:Len>(a:f32, b:&'a Array<T,N>) -> BoxVector<'a,T,N> where f32:Mul<T,Output=T> { BoxVector::new(move |i| a*b[i]) }
 
 /// Operator
-//#[allow(non_camel_case_types)] pub type operator<T=f32, const N:Len> = fn(&dyn Vector<T,N>)->BoxVector<T,N>;
-pub trait Operator<T=f32, const N:Len> = Fn(&dyn Vector<T,N>)->BoxVector<T,N>;
-pub trait OperatorOnce<T=f32, const N:Len> = FnOnce(&dyn Vector<T,N>)->BoxVector<T,N>;
-pub type BoxOperator<'a, T=f32, const N:Len> = crate::compose::BoxFn<'a, (&'a dyn Vector<T,N>,), BoxVector<'a,T,N>>;
+//pub trait Operator<T=f32, const N:Len> = Fn(&dyn Vector<T,N>)->BoxVector<T,N>;
+pub trait Operator<T=f32> = Fn(&dyn Vector<T>)->BoxVector<T>;
+//pub trait OperatorOnce<T=f32, const N:Len> = FnOnce(&dyn Vector<T,N>)->BoxVector<T,N>;
+pub trait OperatorOnce<T=f32,> = FnOnce(&dyn Vector<T>)->BoxVector<T>;
+//pub type BoxOperator<'a, T=f32, const N:Len> = crate::compose::BoxFn<'a, (&'a dyn Vector<T,N>,), BoxVector<'a,T,N>>;
+pub type BoxOperator<'a, T=f32> = crate::compose::BoxFn<'a, (&'a dyn Vector<T>,), BoxVector<'a,T>>;
 
 // vector · operator
-impl<'a, T, const N:Len> Mul<&'a dyn Operator<T,N>> for &'a Array<f32,N> where f32:Mul<T,Output=T> {
+/*impl<'a, T, const N:Len> Mul<&'a dyn Operator<T,N>> for &'a Array<f32,N> where f32:Mul<T,Output=T> {
     type Output = BoxOperator<'a,T,N>;
     fn mul(self, B:&'a dyn Operator<T,N>) -> Self::Output { BoxOperator::new(move |v| { let b=B(v); BoxVector::new(move |i| self[i] * b(i)) }) }
-}
-/*impl<'a, T, B:Operator<T,N>, const N:Len> Mul<B> for &'a Array<f32,N> where f32:Mul<T,Output=T> {
-    type Output = BoxOperator<'a,T,N>;
-    //fn mul(self, B:&'a dyn Operator<T,N>) -> Self::Output { BoxOperator::new(move |v| { let b=B(v); BoxVector::new(move |i| self[i] * b(i)) }) }
-    fn mul(self, B:B) -> Self::Output { BoxOperator::new(move |v| { let b=B(v); BoxVector::new(move |i| self[i] * b(i)) }) }
 }*/
-
+impl<'a, T> Mul<&'a dyn Operator<T>> for &'a Array<f32> where f32:Mul<T,Output=T> {
+    type Output = BoxOperator<'a,T>;
+    fn mul(self, B:&'a dyn Operator<T>) -> Self::Output { BoxOperator::new(move |v| { let b=B(v); BoxVector::new(move |i| self[i] * b(i)) }) }
+}
 
 pub trait Dot<T> { fn dot<A:Fn(Idx)->f32, B:Fn(Idx)->T+?Sized>(self, a : &A, b : &B) -> T; }
 impl<T:std::iter::Sum<<f32 as Mul<T>>::Output>> Dot<T> for std::ops::Range<Idx> where f32:Mul<T> {
@@ -48,18 +62,30 @@ impl<T:std::iter::Sum<<f32 as Mul<T>>::Output>> Dot<T> for std::ops::Range<Idx> 
 impl<T:std::iter::Sum<<f32 as Mul<T>>::Output>> Dot<T> for std::ops::RangeTo<Idx> where f32:Mul<T> {
     fn dot<A:Fn(Idx)->f32, B:Fn(Idx)->T+?Sized>(self, a : &A, b : &B) -> T { (0..self.end).dot(a,b) }
 }
-pub trait DotN<T> { fn dot<A:Fn(Idx)->f32, B:Fn(Idx)->T+?Sized,const N:Len>(self, a : &A, b : &B) -> T; }
+/*pub trait DotN<T> { fn dot<A:Fn(Idx)->f32, B:Fn(Idx)->T+?Sized,const N:Len>(self, a : &A, b : &B) -> T; }
 impl<T> DotN<T> for std::ops::RangeFrom<Idx> where std::ops::Range<Idx>:Dot<T> {
     fn dot<A:Vector<f32,N>, B:Vector<T,N>+?Sized, const N:Len>(self, a : &A, b : &B) -> T { (self.start..N).dot(a,b) }
 }
 impl<T> DotN<T> for std::ops::RangeFull where std::ops::RangeFrom<Idx>:DotN<T> {
     fn dot<A:Vector<f32,N>, B:Vector<T,N>+?Sized, const N:Len>(self, a : &A, b : &B) -> T { (0..).dot(a,b) }
 }
-pub fn dot<T,A:Vector<f32,N>, B:Vector<T,N>+?Sized, const N:Len>(a : &A, b : &B) -> T where std::ops::RangeFull:DotN<T> { (..).dot(a,b) }
+pub fn dot<T,A:Vector<f32,N>, B:Vector<T,N>+?Sized, const N:Len>(a : &A, b : &B) -> T where std::ops::RangeFull:DotN<T> { (..).dot(a,b) }*/
+//pub fn dot<T,A:Vector<f32>, B:Vector<T>+?Sized, const N:Len>(a : &A, b : &B) -> T where std::ops::RangeTo<Idx>:Dot<T> { (..N).dot(a,b) }
+pub fn dot<T,A:Vector<f32>, B:Vector<T>+?Sized>(N:Len, a : &A, b : &B) -> T where std::ops::RangeTo<Idx>:Dot<T> { (..N).dot(a,b) }
 
-pub trait Matrix<const N:Len> = Fn(Idx,Idx)->f32;
+/*pub trait Matrix<const N:Len> = Fn(Idx,Idx)->f32;
 pub fn matrix_mul<'a, A:Matrix<N>+'a,T:std::iter::Sum,B:Vector<T,N>+?Sized,const N:Len>(a:A, b:&'a B) -> impl Vector<T,N> + 'a where f32:Mul<T,Output=T> {
     move |i| dot(&|j| a(i, j), b)
+}*/
+/*pub fn matrix_mul<'a, A:Matrix<N>+'a,T:std::iter::Sum,B:Vector<T>+?Sized,const N:Len>(a:A, b:&'a B) -> impl Vector<T> + 'a where f32:Mul<T,Output=T> {
+    move |i| dot::<_,_,_,N>(&|j| a(i, j), b)
+}*/
+pub trait Matrix = Fn(Idx,Idx)->f32;
+/*pub fn matrix_mul<'a, A:Matrix+'a,T:std::iter::Sum,B:Vector<T>+?Sized, const N:Len>(a:A, b:&'a B) -> impl Vector<T> + 'a where f32:Mul<T,Output=T> {
+    move |i| dot::<_,_,_,N>(&|j| a(i, j), b)
+}*/
+pub fn matrix_mul<'a, A:Matrix+'a,T:std::iter::Sum,B:Vector<T>+?Sized>(N:Len, a:A, b:&'a B) -> impl Vector<T> + 'a where f32:Mul<T,Output=T> {
+    move |i| dot(N, &|j| a(i, j), b)
 }
 
 pub struct SparseVector<T=f32,const N : Idx> {
@@ -84,39 +110,54 @@ impl<T:Copy+Zero,const N:Len> FnOnce<(Idx,)> for SparseVector<T,N> { type Output
 impl<T:Copy+Zero,const N:Len> FnMut<(Idx,)> for SparseVector<T,N> { extern "rust-call" fn call_mut(&mut self, args: (Idx,)) -> Self::Output { self.call(args) } }
 impl<T:Copy+Zero,const N:Len> Fn<(Idx,)> for SparseVector<T,N> { extern "rust-call" fn call(&self, args: (Idx,)) -> Self::Output { self.get(args.0).unwrap_or(T::zero()) } }
 
-pub struct CSC<T=f32,const N : Idx> { columns : [SparseVector<T,N>; N as usize] }
+pub struct CSC<T=f32,const N : Idx> where [SparseVector<T,N>;N]: { columns : [SparseVector<T,N>; N] }
+//pub struct CSC<T=f32,const N : Idx> { columns : Vec<SparseVector<T,N>> }
 impl<T,const N : Idx> Zero for CSC<T,N> { fn zero() -> Self { Self{columns: framework::core::array::collect(|_|Default::default())} } }
+//impl<T,const N : Idx> Zero for CSC<T,N> { fn zero() -> Self { Self{columns: (0..N).map(|_|Default::default()).collect()} } }
 
 impl<T:Copy+Zero,const N : Idx> CSC<T,N> {
     fn column(&self, j : Idx) -> &SparseVector<T,N> { &self.columns[j as usize] }
     fn column_mut(&mut self, j : Idx) -> &mut SparseVector<T,N> { &mut self.columns[j as usize] }
-    fn line(&self, i : Idx) -> impl Vector<T,N> + '_ { move |j| self(i, j) }
     fn get(&self, i : Idx, j : Idx) -> Option<T> { self.column(j).get(i) }
-
+    fn value(&self, i : Idx, j : Idx) -> T { self.get(i, j).unwrap_or(T::zero()) }
     fn insert(&mut self, i:Idx, j:Idx, v:T) -> Option<T> { self.column_mut(j).insert(i, v) }
     fn insert_once(&mut self, i:Idx, j:Idx, v:T) where T:std::fmt::Debug { self.insert(i, j, v).unwrap_none() }
 }
 
-impl<T:Copy+Zero,const N:Len> FnOnce<(Idx,Idx)> for CSC<T,N> { type Output=T; extern "rust-call" fn call_once(self, args: (Idx,Idx)) -> Self::Output { self.call(args) } }
+/*impl<T:Copy+Zero,const N:Len> FnOnce<(Idx,Idx)> for CSC<T,N> { type Output=T; extern "rust-call" fn call_once(self, args: (Idx,Idx)) -> Self::Output { self.call(args) } }
 impl<T:Copy+Zero,const N:Len> FnMut<(Idx,Idx)> for CSC<T,N> { extern "rust-call" fn call_mut(&mut self, args: (Idx,Idx)) -> Self::Output { self.call(args) } }
-impl<T:Copy+Zero,const N:Len> Fn<(Idx,Idx)> for CSC<T,N> { extern "rust-call" fn call(&self, args: (Idx,Idx)) -> Self::Output { self.get(args.0, args.1).unwrap_or(T::zero()) } }
+impl<T:Copy+Zero,const N:Len> Fn<(Idx,Idx)> for CSC<T,N> { extern "rust-call" fn call(&self, args: (Idx,Idx)) -> Self::Output { self.value(args.0, args.1)} }*/
+
+impl<T:Copy+Zero,const N : Idx> CSC<T,N> {
+    //fn line(&self, i : Idx) -> impl Vector<T,N> + '_ { move |j| self(i, j) }
+    fn line(&self, i : Idx) -> impl Vector<T> + '_ { move |j| self.value(i, j) }
+}
 
 pub type LU<const N:Len> = CSC<f32,N>;
 impl<const N:Len> LU<N> {
-    pub fn new<M:Matrix<N>>(A : M) -> Self {
+    //pub fn new<M:Matrix<N>>(A : M) -> Self {
+    pub fn new<M:Matrix>(A : M) -> Self {
         let mut LU = CSC::zero();
         for i in 0..N {
             for j in i..N { let v = A(i, j) - (..i).dot(&LU.line(i), LU.column(j)); LU.insert_once(i, j, v);  }
-            for j in i+1..N { let v = (1. / LU(i, i)) * (A(j, i) - (..i).dot(&LU.line(i), LU.column(j))); LU.insert_once(j, i, v); }
+            for j in i+1..N { let v = (1. / LU.get(i, i).unwrap()) * (A(j, i) - (..i).dot(&LU.line(i), LU.column(j))); LU.insert_once(j, i, v); }
         }
         LU // L+U-I
     }
-    pub fn solve<T:Zero+Copy+std::iter::Sum+std::ops::Sub<Output=T>,B:Vector<T,N_>, const N_:Idx>(&self, b:B) -> Array<T,N_> where f32:Mul<T,Output=T> {
+    /*pub fn solve<T:Zero+Copy+std::iter::Sum+std::ops::Sub<Output=T>,B:Vector<T,N_>, const N_:Idx>(&self, b:B) -> Array<T,N_> where f32:Mul<T,Output=T> {
         assert_eq!(N, N_);
         let mut y : Array<T,N> = Zero::zero(); // Ly = b (uninit ok)
         for i in 0..N { y[i] = b(i) - (..i).dot(&self.line(i), &y); }
         let mut x : Array<T,N_> = Zero::zero(); // Ux = y (uninit ok)
         for i in N-1..=0 { x[i] = (1. / self(i, i)) * (y[i] - (i+1..N).dot(&self.line(i), &x)); }
+        x
+    }*/
+    pub fn solve<T:Zero+Copy+std::iter::Sum+std::ops::Sub<Output=T>,B:Vector<T>>(&self, b:B) -> Array<T> where f32:Mul<T,Output=T> {
+        //let mut y : Array<T> = zero(); // Ly = b (uninit ok)
+        let mut y : Array<T> = zero::<_,N>(); // Ly = b (uninit ok)
+        for i in 0..N { y[i] = b(i) - (..i).dot(&self.line(i), &y); }
+        let mut x : Array<T> = zero::<_,N>(); // Ux = y (uninit ok)
+        for i in N-1..=0 { x[i] = (1. / self.get(i, i).unwrap()) * (y[i] - (i+1..N).dot(&self.line(i), &x)); }
         x
     }
 }
--- src/mesh.rs
+++ src/mesh.rs
@@ -1,67 +1,132 @@
-use framework::vector::{size2,uint2,int2};
-
+use framework::vector::size2;
 pub type Mesh = size2;
 pub const fn N(M:Mesh) -> algebra::Len { (M.x*M.y) as algebra::Len }
+//pub const fn N(Mx:u32,My:u32) -> algebra::Len { (Mx*My) as algebra::Len }
 fn div_remu(n : u32, d : u32) -> (u32, u32) { (n/d, n%d) }
 fn div_rem(n : i32, d : u32) -> (i32, i32) { (n/d as i32, n%d as i32) }
-use algebra::Idx;
-pub fn mesh<const M:Mesh>(i:Idx) -> uint2 { div_remu(i as u32,M.x).into() }
-pub fn dmesh<const M:Mesh>(i:Idx,j:Idx) -> int2 { div_rem(j as i32 - i as i32, M.x).into() }
+use framework::vector::{uint2,int2}; use algebra::Idx;
+//pub fn mesh<const M:Mesh>(i:Idx) -> uint2 { div_remu(i as u32,M.x).into() }
+pub fn mesh(M:Mesh, i:Idx) -> uint2 { div_remu(i as u32,M.x).into() }
+//pub fn dmesh<const M:Mesh>(i:Idx,j:Idx) -> int2 { div_rem(j as i32 - i as i32, M.x).into() }
+pub fn dmesh(M:Mesh, i:Idx,j:Idx) -> int2 { div_rem(j as i32 - i as i32, M.x).into() }
 
 use crate::algebra;
-pub type Field<T=f32,const M:Mesh> = algebra::Array<T,{N(M)}>;
+
+//pub type Field<T=f32,const M:Mesh> = algebra::Array<T,{N(M)}>;
+pub type Field<T=f32,const M:Mesh> = algebra::Array<T>;
+pub fn zero<T:framework::core::Zero, const M:Mesh>() -> Field<T,M> { algebra::zero::<T,{N(M)}>() }
+
 pub type Matrix<'a, const M:Mesh> = crate::compose::BoxFn<'a,(Idx, Idx),f32>;
-pub type Operator<T,const M:Mesh> = Box<dyn algebra::Operator<T,{N(M)}>>;
-pub type OperatorOnce<T,const M:Mesh> = Box<dyn algebra::OperatorOnce<T,{N(M)}>>;
+//fn matrix<F:Fn(Mesh,uint2,int2)->f32, const M:Mesh>(f : F) -> impl Fn(Idx,Idx)->f32 { move |i,j| { f(M, mesh::<M>(i), dmesh::<M>(i,j)) } }
+fn matrix<F:Fn(Mesh,uint2,int2)->f32>(M:Mesh, f : F) -> impl Fn(Idx,Idx)->f32 { move |i,j| { f(M, mesh(M,i), dmesh(M,i,j)) } }
+
+//pub type Operator<T,const M:Mesh> = Box<dyn algebra::Operator<T,{N(M)}>>;
+pub type Operator<T,const M:Mesh> = Box<dyn algebra::Operator<T>>;
+//pub type Operator<T,const MX:u32, const MY:u32> = Box<dyn algebra::Operator<T,{N(MX,MY)}>>;
+/*pub fn operator<T:std::iter::Sum,F:Fn(uint2,int2)->f32+Clone+'static,const M:Mesh>(f : F) -> Operator<T,M> where f32:std::ops::Mul<T,Output=T> {
+    box move |v| algebra::BoxVector::new(algebra::matrix_mul::<_,_,_,{N(M)}>(matrix::<_,M>(f.clone()),v))
+}*/
+/*pub fn operator<T:std::iter::Sum,F:Fn(Mesh,uint2,int2)->f32+Clone+'static,const M:Mesh>(f : F) -> Operator<T,M> where f32:std::ops::Mul<T,Output=T> {
+    box move |v| algebra::BoxVector::new(algebra::matrix_mul::<_,_,_,{N(M)}>(matrix::<_,M>(f.clone()),v))
+}*/
+/*pub fn operator<T:std::iter::Sum,F:Fn(Mesh,uint2,int2)->f32+Clone+'static,const M:Mesh>(f : F) -> Operator<T,M> where f32:std::ops::Mul<T,Output=T> {
+    box move |v| algebra::BoxVector::new(algebra::matrix_mul(N(M),matrix::<_,M>(f.clone()),v))
+}*/
+pub fn operator<T:std::iter::Sum,F:Fn(Mesh,uint2,int2)->f32+Clone+'static,const M:Mesh>(f : F) -> Operator<T,M> where f32:std::ops::Mul<T,Output=T> {
+    box move |v| algebra::BoxVector::new(algebra::matrix_mul(N(M),matrix(M,f.clone()),v))
+}
+
+//pub type OperatorOnce<T,const M:Mesh> = Box<dyn algebra::OperatorOnce<T,{N(M)}>>;
+//pub type OperatorOnce<T,const MX:u32, const MY:u32> = Box<dyn algebra::OperatorOnce<T,{N(MX,MY)}>>;
+pub type OperatorOnce<T,const M:Mesh> = Box<dyn algebra::OperatorOnce<T>>;
+//pub type OperatorOnce<T> = Box<dyn algebra::OperatorOnce<T>>;
+/*fn operator_once<T:std::iter::Sum, F:Fn(Mesh,uint2,int2)->f32+'static, const M:Mesh>(f : F) -> OperatorOnce<T,M> where f32:std::ops::Mul<T,Output=T> {
+    box move |v| algebra::BoxVector::new(algebra::matrix_mul::<_,_,_,{N(M)}>(matrix::<_,M>(f),v))
+}*/
+/*fn operator_once<T:std::iter::Sum, F:Fn(Mesh,uint2,int2)->f32+'static, const M:Mesh>(f : F) -> OperatorOnce<T,M> where f32:std::ops::Mul<T,Output=T> {
+    box move |v| algebra::BoxVector::new(algebra::matrix_mul(N(M),matrix::<_,M>(f),v))
+}*/
+fn operator_once<T:std::iter::Sum, F:Fn(Mesh,uint2,int2)->f32+'static, const M:Mesh>(f : F) -> OperatorOnce<T,M> where f32:std::ops::Mul<T,Output=T> {
+    box move |v| algebra::BoxVector::new(algebra::matrix_mul(N(M),matrix(M,f),v))
+}
+
 pub struct Equation<T=f32,const M:Mesh> {pub A : algebra::LU<{N(M)}>, pub B : Box<dyn Fn()->Box<OperatorOnce<T,M>>>} // Ax = Bx' + ...
+//pub struct Equation<T=f32,const M:Mesh> {pub A : algebra::LU<{N(M)}>, pub B : Box<dyn Fn()->Box<OperatorOnce<T>>>} // Ax = Bx' + ...
 
-fn matrix<F:Fn(uint2, int2)->f32, const M:Mesh>(f : F) -> impl Fn(Idx,Idx)->f32 { move |i,j| { f(mesh::<M>(i), dmesh::<M>(i,j)) } }
+impl<T:std::iter::Sum,const M:Mesh> Equation<T,M> {
+    pub fn new<A:Fn(Mesh,uint2,int2)->f32, B:Fn()->Box<dyn Fn(Mesh,uint2,int2)->f32>+'static>(A:A, B:B) -> Self where f32:std::ops::Mul<T,Output=T> {
+        //Self{ A: algebra::LU::new(matrix::<_,M>(A)), B: box move ||box operator_once::<_,_,M>(B()) }
+        Self{ A: algebra::LU::new(matrix(M,A)), B: box move ||box operator_once::<_,_,M>(B()) }
+    }
+}
 
-use framework::{core::{mask,sign,abs,sq}, vector::{xy,vec2}};
+use framework::{core::{mask,sign,abs,sq}, vector::xy};
 pub type BC = fn(u32,u32,i32)->f32;
 
-pub trait Operators<const M:Mesh> {
-    /*fn operator<T:std::iter::Sum,const f:fn(uint2,int2)->f32>(v:&dyn algebra::Vector<T,{M(N)}>) -> algebra::BoxVector<T,M> where f32:std::ops::Mul<T,Output=T> {
+//pub trait Operators<const M:Mesh> {
+//pub trait Operators<const MX:u32, const MY:u32> {
+    /*fn const_operator<T:std::iter::Sum,const f:fn(uint2,int2)->f32>(v:&dyn algebra::Vector<T,{M(N)}>) -> algebra::BoxVector<T,M> where f32:std::ops::Mul<T,Output=T> {
         algebra::BoxVector::new(algebra::matrix_mul::<_,_,_,{N(M)}>(matrix::<_,M>(&f),v))
-    }
-    fn eq<T:std::iter::Sum,A:Fn(uint2,int2)->f32, const B:fn(uint2,int2)->f32>(A:A) -> Equation<T,M> where f32:std::ops::Mul<T,Output=T> {
+    }*/
+    /*fn operator<T:std::iter::Sum,F:Fn(uint2,int2)->f32+Clone+'static>(f : F) -> Operator<T,{xy{x:MX,y:MY}}> where f32:std::ops::Mul<T,Output=T> {
+        box move |v| algebra::BoxVector::new(algebra::matrix_mul::<_,_,_,{N(MX,MY)}>( matrix::<_,MX,MY>(f.clone()),v))
+    }*/
+    /*fn operator_once<T:std::iter::Sum,F:Fn(uint2,int2)->f32+'static>(f : F) -> OperatorOnce<T,{xy{x:MX,y:MY}}> where f32:std::ops::Mul<T,Output=T> {
+        box move |v| algebra::BoxVector::new(algebra::matrix_mul::<_,_,_,{N(MX,MY)}>(matrix::<_,MX,MY>(f),v))
+    }*/
+    /*fn eq<T:std::iter::Sum,A:Fn(uint2,int2)->f32, const B:fn(uint2,int2)->f32>(A:A, B:B) -> Equation<T,M> where f32:std::ops::Mul<T,Output=T> {
         Equation{ A: algebra::LU::new(matrix::<_,M>(A)), B: Self::operator::<B> }
     }*/
-    fn operator<T:std::iter::Sum,F:Fn(uint2,int2)->f32+Clone+'static>(f : F) -> Operator<T,M> where f32:std::ops::Mul<T,Output=T> {
-        box move |v| algebra::BoxVector::new(algebra::matrix_mul::<_,_,_,{N(M)}>(matrix::<_,M>(f.clone()),v))
-    }
-    fn operator_once<T:std::iter::Sum,F:Fn(uint2,int2)->f32+'static>(f : F) -> OperatorOnce<T,M> where f32:std::ops::Mul<T,Output=T> {
-        box move |v| algebra::BoxVector::new(algebra::matrix_mul::<_,_,_,{N(M)}>(matrix::<_,M>(f),v))
-    }
-    fn eq<T:std::iter::Sum,A:Fn(uint2,int2)->f32, B:Fn()->Box<dyn Fn(uint2,int2)->f32>+'static>(A:A, B:B) -> Equation<T,M> where f32:std::ops::Mul<T,Output=T> {
-        Equation{ A: algebra::LU::new(matrix::<_,M>(A)), B: box move ||box Self::operator_once(B()) }
-    }
-
+    /*fn eq<T:std::iter::Sum,A:Fn(uint2,int2)->f32, B:Fn()->Box<dyn Fn(uint2,int2)->f32>+'static>(A:A, B:B) -> Equation<T,MX,MY> where f32:std::ops::Mul<T,Output=T> {
+        Equation{ A: algebra::LU::new(matrix::<_,MX,MY>(A)), B: box move ||box Self::operator_once(B()) }
+    }*/
+    /*fn eq<T:std::iter::Sum,A:Fn(Mesh,uint2,int2)->f32, B:Fn()->Box<dyn Fn(Mesh,uint2,int2)->f32>+'static,const N:algebra::Len>(M:Mesh, A:A, B:B) -> Equation<T,N> where f32:std::ops::Mul<T,Output=T> {
+        Equation{ A: algebra::LU::new(matrixM(M,A)), B: box move ||box Self::operator_once(M,B()) }
+    }*/
     //fn I(_:uint2, d:int2) -> f32 { mask(d==0, 1.) }
-    fn I()->crate::compose::BoxFn<'static,(uint2,int2),f32>{crate::compose::BoxFn::new(|_,d| mask(d==0, 1.) )}
-    fn border(xy{x,y}:uint2) -> bool { x==0 || x==M.x-1 || y==0 || y==M.y-1 }
-    fn interior(p:uint2,predicate:bool,value:f32) -> f32 { mask(predicate && !Self::border(p), value) }
+    //pub fn I<const M:Mesh>()->crate::compose::BoxFn<'static,(uint2,int2),f32>{crate::compose::BoxFn::new(|_,d| mask(d==0, 1.) )}
+    pub fn I()->crate::compose::BoxFn<'static,(Mesh,uint2,int2),f32>{crate::compose::BoxFn::new(|_,_,d| mask(d==0, 1.) )}
+    //fn border<const M:Mesh>(xy{x,y}:uint2) -> bool { x==0 || x==M.x-1 || y==0 || y==M.y-1 }
+    fn border(M:Mesh, xy{x,y}:uint2) -> bool { x==0 || x==M.x-1 || y==0 || y==M.y-1 }
+    //fn interior<const M:Mesh>(p:uint2,predicate:bool,value:f32) -> f32 { mask(predicate && !border::<M>(p), value) }
+    fn interior(M:Mesh, p:uint2, predicate:bool, value:f32) -> f32 { mask(predicate && !border(M,p), value) }
     //fn P(p:uint2, d:int2) -> f32 { Self::interior(p, d==0, 1.) }
-    fn P()->crate::compose::BoxFn<'static,(uint2,int2),f32>{crate::compose::BoxFn::new(|p,d| Self::interior(p, d==0, 1.) )}
+    //pub fn P<const M:Mesh>()->crate::compose::BoxFn<'static,(uint2,int2),f32>{crate::compose::BoxFn::new(|p,d| interior::<M>(p, d==0, 1.) )}
+    pub fn P()->crate::compose::BoxFn<'static,(Mesh,uint2,int2),f32>{crate::compose::BoxFn::new(|M,p,d| interior(M, p, d==0, 1.) )}
 
-    #[allow(non_upper_case_globals)] const δ : vec2 = framework::vector::div_f32(1f32, M.as_f32());
-    fn Dx(p:uint2, d:int2) -> f32 { Self::interior(p, (abs(d.x),d.y) == (1,0), sign(d.x) as f32/(2.*Self::δ.x)) } // ∂x
-    fn Dy(p:uint2, d:int2) -> f32 { Self::interior(p, (d.x,abs(d.y)) == (0,1), sign(d.y) as f32/(2.*Self::δ.y)) } // ∂y
+    //#[allow(non_upper_case_globals)] fn δ<const M:Mesh>() -> framework::vector::vec2 { framework::vector::div_f32(1f32, M.as_f32()) }
+    fn δ(M:Mesh) -> framework::vector::vec2 { framework::vector::div_f32(1f32, M.as_f32()) }
+    //pub fn Dx<const M:Mesh>(p:uint2, d:int2) -> f32 { interior::<M>(p, (abs(d.x),d.y) == (1,0), sign(d.x) as f32/(2.*δ::<M>().x)) } // ∂x
+    pub fn Dx(M:Mesh, p:uint2, d:int2) -> f32 { interior(M,p, (abs(d.x),d.y) == (1,0), sign(d.x) as f32/(2.*δ(M).x)) } // ∂x
+    //pub fn Dy<const M:Mesh>(p:uint2, d:int2) -> f32 { interior::<M>(p, (d.x,abs(d.y)) == (0,1), sign(d.y) as f32/(2.*δ::<M>().y)) } // ∂y
+    pub fn Dy(M:Mesh, p:uint2, d:int2) -> f32 { interior(M,p, (d.x,abs(d.y)) == (0,1), sign(d.y) as f32/(2.*δ(M).y)) } // ∂y
     //fn Δ(p:uint2, d:int2) -> f32 {
-    fn Δ()->crate::compose::BoxFn<'static,(uint2,int2),f32>{crate::compose::BoxFn::new(|p,d:int2|{
-        Self::interior(p, true, {
+    //pub fn Δ<const M:Mesh>()->crate::compose::BoxFn<'static,(uint2,int2),f32>{crate::compose::BoxFn::new(|p,d:int2|{
+    pub fn Δ()->crate::compose::BoxFn<'static,(Mesh,uint2,int2),f32>{crate::compose::BoxFn::new(|M:Mesh,p,d:int2|{
+        //interior::<M>(p, true, {
+        interior(M,p, true, {
             match (abs(d.x),abs(d.y)) {
-                (0,0) => -2.*(1./sq(Self::δ.x)+1./sq(Self::δ.y)),
-                (1,0) => 1./sq(Self::δ.x),
-                (0,1) => 1./sq(Self::δ.y),
+                //(0,0) => -2.*(1./sq(Self::δ.x)+1./sq(Self::δ.y)),
+                (0,0) => -2.*(1./sq(δ(M).x)+1./sq(δ(M).y)),
+                //(1,0) => 1./sq(Self::δ.x),
+                (1,0) => 1./sq(δ(M).x),
+                //(0,1) => 1./sq(Self::δ.y),
+                (0,1) => 1./sq(δ(M).y),
                 _ => 0.
             }
         })
     })}
 
-    fn BC<const X: BC, const Y: BC>(p:uint2, d:int2) -> f32 {
+    pub fn BC<const M:Mesh, const X: BC, const Y: BC>(p:uint2, d:int2) -> f32 {
         if p.x==0 || p.x==M.x-1 { X(M.x,p.x,d.x) } // Horizontal boundary condition kernel (and corners)
         else if p.y==0 || p.y==M.y-1 { Y(M.y,p.y,d.y) }
         else { 0. }
     }
-}
+
+#[macro_export] macro_rules! BC { ($X:ident, $Y:ident) => (
+    {fn BC(M:Mesh, p:uint2, d:int2) -> f32 {
+        if p.x==0 || p.x==M.x-1 { $X(M.x,p.x,d.x) } // Horizontal boundary condition kernel (and corners)
+        else if p.y==0 || p.y==M.y-1 { $Y(M.y,p.y,d.y) }
+        else { 0. }
+    } BC }
+)}
